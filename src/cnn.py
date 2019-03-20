b6fb3b18a45525a357e6e1799ab90bc4e53bd290
Yago Henrique <yago.trarbach@cscservicos.com.br>
2019-03-20 05:16:58 -0300

    Adicionado mais uma hidden layer e mais epochs

--- src/cnn.py
+++ src/cnn.py
@@ -31,11 +31,15 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# Adding a third convolutional layer
+classifier.add(Conv2D(64, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 64, activation = 'relu'))
 classifier.add(Dense(units = 5, activation = 'softmax'))
 
 # Compiling the CNN
@@ -48,6 +52,7 @@ from keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
+                                   rotation_range=90,
                                    horizontal_flip = True)
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
@@ -63,10 +68,10 @@ test_set = test_datagen.flow_from_directory('./dataset_turtles/test_set',
                                             class_mode = 'categorical')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 600,
-                         epochs = 10,
+                         steps_per_epoch = 8000/32,
+                         epochs = 100,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 2000/32)
 
 import numpy as np
 from keras.preprocessing import image
